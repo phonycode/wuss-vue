95686442da7c9bf1b0f8541dd36536a00e2f4979
BoWang <471269081@qq.com>
2019-04-24 18:23:28 +0800

    buttons

--- examples/App.vue
+++ examples/App.vue
@@ -1,7 +1,31 @@
 <template>
   <div id="app">
     <wuss-button :fullLine="true">default</wuss-button>
-    <wuss-button>default</wuss-button>
+    <wuss-button :fullLine="true" @click="handleClick" type="primary">primary</wuss-button>
+    <wuss-button :fullLine="true" type="warning">warning</wuss-button>
+    <wuss-button :fullLine="true" type="success">success</wuss-button>
+    <wuss-button :fullLine="true" type="error">error</wuss-button>
+    <wuss-button size="large">large</wuss-button>
+    <wuss-button size="large" type="primary">primary</wuss-button>
+    <wuss-button size="large" type="success">success</wuss-button>
+    <br>
+    <wuss-button >default</wuss-button>
+    <wuss-button type="primary">primary</wuss-button>
+    <wuss-button type="success">success</wuss-button>
+    <br>
+    <wuss-button size="mini">mini</wuss-button>
+    <wuss-button size="mini" type="primary">primary</wuss-button>
+    <wuss-button size="mini" type="success">success</wuss-button>
+    <br>
+    <wuss-button size="mini" :circle="true">1</wuss-button>
+    <wuss-button :circle="true" type="primary">2</wuss-button>
+    <wuss-button size="large" :circle="true" type="success">3</wuss-button>
+    <br>
+    <wuss-button :fullLine="true" :disabled="true">default</wuss-button>
+    <wuss-button :fullLine="true" @click="handleClick" :disabled="true" type="primary">primary</wuss-button>
+    <wuss-button :fullLine="true" :disabled="true" type="warning">warning</wuss-button>
+    <wuss-button :fullLine="true" :disabled="true" type="success">success</wuss-button>
+    <wuss-button :fullLine="true" :disabled="true" type="error">error</wuss-button>
   </div>
 </template>
 
@@ -9,6 +33,9 @@
 
 export default {
   name: 'app',
+  methods: {
+    handleClick() {}
+  }
 }
 </script>
 
@@ -17,7 +44,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -2,7 +2,8 @@
   <button
   :disabled="buttonDisabled"
    class="wuss-button" 
-   @click="handleClick"
+   @touchstart.stop="handleClick"
+   @touchend="handleTouchEnd"
    :class="[
       type ? 'wuss-button--' + type : '',
       buttonSize ? 'wuss-button--' + buttonSize : '',
@@ -10,8 +11,8 @@
         'is-disabled': buttonDisabled,
         'is-loading': loading,
         'is-fullLine': fullLine,
-        'is-round': round,
-        'is-circle': circle
+        'is-circle': circle,
+        'is-active': active
       }
     ]">
     <span><slot></slot></span>
@@ -28,17 +29,39 @@ export default {
       default: () => {
         return false;
       }
+    },
+    size: String,
+    loading: Boolean,
+    circle: {
+      type: Boolean,
+      default: () => {
+        return false;
+      }
+    },
+  },
+  data() {
+    return {
+      active: false,
     }
   },
   computed:{
     buttonDisabled() {
-      return this.disabled
+      return this.disabled === true ? true : false;
+    },
+    buttonSize() {
+      return this.size
     }
   },
   methods: {
     handleClick(evt) {
+      if(!this.buttonDisabled){
+        this.active = true
         this.$emit('click', evt);
-      }
+      }   
+    },
+    handleTouchEnd() {
+      this.active = false
+    }
   }
 }
 </script>
@@ -46,7 +69,6 @@ export default {
   .wuss-button {
     background-color: #fff;
     height: 41px;
-    padding: 0 10px;
     border-color: transparent;
     background-color: #f6f8fa;
     box-shadow: 0 0 1px #b8bbbf;
@@ -56,15 +78,71 @@ export default {
     font-weight: 400;
     font-family: Arial, "Microsoft YaHei";
     text-align: center;
+    margin: 5px;
+    font-size: 18px;
+    box-sizing: border-box;
   }
   .wuss-button:active {
     background-color:#b8bbbf;
   }
   .wuss-button.is-fullLine{
     width: 100%;
+    margin: 5px 0;
   }
-  .wuss-button span {
-    font-size: 18px;
+  /* 圆形 */
+  .wuss-button.is-circle.wuss-button--large{
+    width: 49px;
+    border-radius: 49px;
+  }
+  .wuss-button.is-circle.wuss-button--mini{
+    width: 33px;
+    border-radius: 33px;
+  }
+  .wuss-button.is-circle{
+    width: 41px;
+    border-radius: 41px;
+  }
+  /* 禁止 */
+  .wuss-button.is-disabled{
+    opacity: .6;
+  }
+  /* 大小 */
+  .wuss-button.wuss-button--large{
+    height: 49px;
+    font-size: 20px;
+  }
+  .wuss-button.wuss-button--mini{
+    height: 33px;
+    font-size: 14px;
+  }
+  /* 类型 */
+  .wuss-button.wuss-button--primary{
+    background-color:#26a2ff;
+    color: #fff;
+  }
+  .wuss-button.wuss-button--primary.is-active{
+    background-color:#2086d4;
+  }
+  .wuss-button.wuss-button--warning{
+    background-color:#e6a23c;
+    color: #fff;
+  }
+  .wuss-button.wuss-button--warning.is-active{
+    background-color:#b88230;
+  }
+  .wuss-button.wuss-button--success{
+    background-color:#67c23a;
+    color: #fff;
+  }
+  .wuss-button.wuss-button--success.is-active{
+    background-color:#549e2f;
+  }
+  .wuss-button.wuss-button--error{
+    background-color:#f56c6c;
+    color: #fff;
+  }
+  .wuss-button.wuss-button--error.is-active{
+    background-color:#c45757;
   }
 </style>
 
