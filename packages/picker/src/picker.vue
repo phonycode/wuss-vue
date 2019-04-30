cd9af60a4829afa565f512bb808ef5ab6296c370
whoisfreedom <471269081@qq.com>
2019-04-30 19:19:44 +0800

    placeholder

--- packages/picker/src/picker.vue
+++ packages/picker/src/picker.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="w-picker-input" @touchstart="showPicker">
-      <span v-if="pickLabel.length < 1">请选择</span>
+      <span v-if="pickLabel.length < 1">{{placeholder}}</span>
       <span v-else>{{pickLabel.join(joinCon)}}</span>
       <svg class="icon" aria-hidden="true"> <use xlink:href="#wuss-icon-arrow-right"></use></svg>
     </div>
@@ -65,6 +65,10 @@ export default {
       default: () => []
     },
     joinCon: {
+      type: String,
+      default: ','
+    },
+    placeholder: {
       type: String,
       default: ','
     },
