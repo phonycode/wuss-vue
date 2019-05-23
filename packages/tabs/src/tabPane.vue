c93f142d5592dfbf0749d8561ed6ab7919348fa3
whoisfreedom <471269081@qq.com>
2019-05-23 21:04:24 +0800

    change'

--- packages/tabs/src/tabPane.vue
+++ packages/tabs/src/tabPane.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class='wuss-tab-pane' @touchstart.stop="handleClick" :class="{'tab-pane-active':isActive }">
+  <div class='wuss-tab-pane'
+       @touchstart.stop="handleClick"
+       :class="{'tab-pane-active':isActive }">
     {{label}}
   </div>
 </template>
@@ -8,38 +10,38 @@ export default {
   name: 'WTabPane',
   props: {
     label: String,
-    name: [String,Number],
+    name: [String, Number],
   },
   computed: {
-    isActive: function() {
+    isActive: function () {
       return this.$parent.activeName === this.name
     }
   },
   methods: {
-    handleClick() {
+    handleClick () {
       this.$parent.activeName = this.name
     },
   }
 }
 </script>
 <style>
-  .wuss-tab-pane {
-    position: relative;
-    z-index: 2;
-  }
-  .wuss-tab-pane.tab-pane-active {
-    color: rgb(69, 143, 246);
-  }
-  .wuss-tab-pane.tab-pane-active:before {
-    content: '';
-    display: block;
-    width: 100%;
-    bottom: 0;
-    position: absolute;
-    height: 1px;
-    background-color: rgb(69, 143, 246);
-    z-index: 2;
-  }
+.wuss-tab-pane {
+  position: relative;
+  z-index: 2;
+}
+.wuss-tab-pane.tab-pane-active {
+  color: rgb(69, 143, 246);
+}
+.wuss-tab-pane.tab-pane-active:before {
+  content: "";
+  display: block;
+  width: 100%;
+  bottom: 0;
+  position: absolute;
+  height: 1px;
+  background-color: rgb(69, 143, 246);
+  z-index: 2;
+}
 </style>
 
 
