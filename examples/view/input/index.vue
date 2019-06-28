aa8f787e6809676c5327f1229eeb1fd4651a3bae
juzhiqiang <1020814597@qq.com>
2019-06-28 17:21:43 +0800

    add input component

---
+++ examples/view/input/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="wuss-box">
+    <h3>Input</h3>
+    <small>输入框</small>
+    <div class="wuss-boxs">
+      <section>
+        <h5 class="title">Default</h5>
+        <div>
+          <w-input class="wuss-demo" v-model="rate" :max="20"></w-input>
+        </div>
+      </section>
+      <section>
+        <h5 class="title">步长设置</h5>
+        <div>
+          <w-input class="wuss-demo" v-model="input2" :step="5"></w-input>
+        </div>
+      </section>
+      <section>
+        <h5 class="title">限制输入范围(2-40)</h5>
+        <div>
+          <w-input class="wuss-demo" v-model="input3" :max="40" :min="2"></w-input>
+        </div>
+      </section>
+      <section>
+        <h5 class="title">只读/禁用</h5>
+        <div>
+          <w-input class="wuss-demo" :disabled="true" v-model="input5"></w-input>
+          <w-input class="wuss-demo" readonly v-model="input6"></w-input>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+section {
+  display: block;
+  margin-bottom: 10px;
+}
+
+.wuss-boxs .title {
+  font-weight: 500;
+  font-size: 14px;
+  color: #666;
+  padding: 10px 10px 8px;
+  margin: 0;
+}
+
+.title-small {
+  color: #999;
+}
+
+.wuss-demo {
+  margin-left: 28px;
+  font-weight: 700;
+  color: #000;
+  font-size: 36px;
+}
+</style>
+
+
+
+<script>
+export default {
+  name: "inputs",
+  data() {
+    return {
+      rate: 9,
+      rate1: 2,
+      input2:8,
+      input3: 2,
+      input5: 6,
+      input6: 5
+    };
+  },
+  created() {},
+  methods: {}
+};
+</script>
+
---
+++ packages/input/src/index.vue
@@ -0,0 +1,86 @@
+<!--
+ * @Author: null
+ * @Email: 1020814597@qq.com
+ * @Date: 2019-06-19 17:30:16
+ * @LastEditors: null
+ * @LastEditTime: 2019-06-28 17:20:52
+ * @Description: 
+ * @form: (0 U 0)
+ * @param {String}          type                  文本类型 支持原生所有input类型
+ * @param {String}          label                 文本标题 默认不生成标题
+ * @param {String|Number}   value                 input值
+ * @param {Boolean}        disabled               是否禁用
+ * @param {Boolean}        readonly               是否只读
+ * @param {Boolean}        required               是否启用必填*符号
+ * @param {Boolean}        claerable              是否启用清除按钮
+
+ -->
+<template>
+  <div class="wuss-input">
+    <div class="wuss-input_label">
+      <slot>
+        <span>*</span>
+      </slot>
+      {{label}}
+    </div>
+    <div class="wuss-input_body">
+      <input
+        class="wuss-input_content"
+        :type="type"
+        :value="123"
+        :disabled="disabled"
+        :readonly="readonly"
+        :inputAlign="s"
+      >
+      <div class="wuss-input_right">
+        <w-icon v-if="claerable" type="close"></w-icon>
+        <slot name="ic"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "WInput",
+  props: {
+    type: {
+      type: String,
+      default: "text"
+    },
+    label: [String, Number],
+    value: {},
+    inputAlign: {},
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    claerable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {
+    s() {
+      return "a";
+    }
+  },
+  watch: {},
+  methods: {}
+};
+</script>
+<style scoped>
+.wuss-input_label{}
+</style>
+
